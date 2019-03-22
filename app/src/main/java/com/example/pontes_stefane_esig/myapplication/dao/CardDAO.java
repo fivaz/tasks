f66de5b7fe8bbaef77cdfb78902647016b3d497b
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-03-22 20:27:35 +0100

    wip moving some methods from subclass to DAO

--- app/src/main/java/com/example/pontes_stefane_esig/myapplication/dao/CardDAO.java
+++ app/src/main/java/com/example/pontes_stefane_esig/myapplication/dao/CardDAO.java
@@ -18,25 +18,30 @@ public class CardDAO extends DAO {
         super(context);
     }
 
+    //TODO use prepared statements
     public List<Card> getAll(Listt listt) {
         String sql = "SELECT * FROM " + TB_CARD_NAME + " WHERE listt_id = " + listt.getId();
         SQLiteDatabase db = getReadableDatabase();
-        Cursor c = db.rawQuery(sql, null);
+        Cursor cursor = db.rawQuery(sql, null);
 
         List<Card> cards = new ArrayList<>();
-        while (c.moveToNext()) {
-            int id = c.getInt(c.getColumnIndex("id"));
-            String name = c.getString(c.getColumnIndex("name"));
-            double points = c.getDouble(c.getColumnIndex("points"));
-            long listt_id = c.getLong(c.getColumnIndex("listt_id"));
-
-            Card card = new Card(id, name, points, listt_id);
-            cards.add(card);
-        }
-        c.close();
+        while (cursor.moveToNext())
+            cards.add(buildCard(cursor));
+        cursor.close();
         return cards;
     }
 
+    @NonNull
+    private Card buildCard(Cursor cursor) {
+        int id = cursor.getInt(cursor.getColumnIndex("id"));
+        String name = cursor.getString(cursor.getColumnIndex("name"));
+        double points = cursor.getDouble(cursor.getColumnIndex("points"));
+        long listt_id = cursor.getLong(cursor.getColumnIndex("listt_id"));
+
+        return new Card(id, name, points, listt_id);
+    }
+
+    //TODO Put this in the class DAO
     public void insert(Card card) {
         SQLiteDatabase db = getWritableDatabase();
         long id = db.insert(TB_CARD_NAME, null, getValues(card));
@@ -51,17 +56,20 @@ public class CardDAO extends DAO {
         data.put("listt_id", card.getListt_id());
         return data;
     }
-    
+
+    //TODO Put this in the class DAO
     public void delete(Card card) {
         SQLiteDatabase db = getWritableDatabase();
         db.delete(TB_CARD_NAME, "id = ?", getPK(card));
     }
 
+    //TODO Put this in the class DAO
     @NonNull
     private String[] getPK(Card card) {
         return new String[]{String.valueOf(card.getId())};
     }
 
+    //TODO Put this in the class DAO
     public void update(Card card) {
         SQLiteDatabase db = getWritableDatabase();
         db.update(TB_CARD_NAME, getValues(card), "id = ?", getPK(card));
