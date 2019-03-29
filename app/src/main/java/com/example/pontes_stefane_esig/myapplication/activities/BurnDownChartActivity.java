f4d08c13ce2f052e4d328a052efe19ca8f5a3642
ESIG-316-19\PONTES_STEFANE-ESIG <elv-stefane.pntsf@eduge.ch>
2019-03-29 17:33:16 +0100

    wip function createBurndownchart

--- app/src/main/java/com/example/pontes_stefane_esig/myapplication/activities/BurnDownChartActivity.java
+++ app/src/main/java/com/example/pontes_stefane_esig/myapplication/activities/BurnDownChartActivity.java
@@ -1,7 +1,9 @@
 package com.example.pontes_stefane_esig.myapplication.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.pontes_stefane_esig.myapplication.R;
 import com.github.mikephil.charting.charts.BarChart;
@@ -20,7 +22,10 @@ import com.github.mikephil.charting.utils.Utils;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.DataFormatException;
+
+import static java.util.Arrays.asList;
 
 public class BurnDownChartActivity extends AppCompatActivity {
 
@@ -28,9 +33,63 @@ public class BurnDownChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_burn_down_chart);
+
+        List<Float> idealData = new ArrayList<>();
+        idealData.add(150f);
+        idealData.add(100f);
+        idealData.add(50f);
+        idealData.add(0f);
+
+        List<Float> actualData = new ArrayList<>();
+        actualData.add(150f);
+        actualData.add(150f);
+        actualData.add(30f);
+        actualData.add(0f);
+
+        buildBurnDownChart(idealData, actualData);
 //        barChart();
-        lineChart();
+//        lineChart();
 //        lineChart2();
+    }
+
+    private void buildBurnDownChart(List<Float> idealData, List<Float> actualData) {
+        Utils.init(this);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+        LineDataSet ideaDataSet = buildIdealDataSet(idealData);
+        LineDataSet actualDataSet = buildActualDataSet(actualData);
+        dataSets.add(ideaDataSet);
+        dataSets.add(actualDataSet);
+
+        LineData data = new LineData(dataSets);
+        LineChart chart = new LineChart(this);
+        setContentView(chart);
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+    @NonNull
+    private LineDataSet buildActualDataSet(List<Float> actualData) {
+        return buildDataSet(actualData, "actual task remaning", R.color.red);
+    }
+
+    @NonNull
+    private LineDataSet buildIdealDataSet(List<Float> idealData) {
+        return buildDataSet(idealData, "ideal task remaning", R.color.blue);
+    }
+
+    private LineDataSet buildDataSet(List<Float> data, String label, int color) {
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++)
+            entries.add(new Entry(i, data.get(i)));
+
+        LineDataSet dataSet = new LineDataSet(entries, label);
+
+        dataSet.setColor(getResources().getColor(color));
+        dataSet.setLineWidth(2.5f);
+        dataSet.setCircleRadius(4f);
+        return dataSet;
     }
 
     private void lineChart() {
@@ -39,12 +98,10 @@ public class BurnDownChartActivity extends AppCompatActivity {
         ArrayList<ILineDataSet> datasets = new ArrayList<>();
 
         ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(0, 4f));
-        entries.add(new Entry(1, 8f));
-        entries.add(new Entry(2, 6f));
-        entries.add(new Entry(3, 12f));
-        entries.add(new Entry(4, 18f));
-        entries.add(new Entry(5, 9f));
+        entries.add(new Entry(0, 150f));
+        entries.add(new Entry(1, 100f));
+        entries.add(new Entry(2, 50f));
+        entries.add(new Entry(3, 0f));
 
         LineDataSet dataset = new LineDataSet(entries, "text");
 
@@ -56,12 +113,10 @@ public class BurnDownChartActivity extends AppCompatActivity {
         datasets.add(dataset);
 
         ArrayList<Entry> entries1 = new ArrayList<>();
-        entries1.add(new Entry(0, 4f));
-        entries1.add(new Entry(1, 8f));
-        entries1.add(new Entry(2, 10f));
-        entries1.add(new Entry(3, 3f));
-        entries1.add(new Entry(4, 18f));
-        entries1.add(new Entry(5, 9f));
+        entries1.add(new Entry(0, 150f));
+        entries1.add(new Entry(1, 150f));
+        entries1.add(new Entry(2, 30f));
+        entries1.add(new Entry(3, 0f));
 
         LineDataSet dataset1 = new LineDataSet(entries1, "text1");
 
@@ -81,8 +136,7 @@ public class BurnDownChartActivity extends AppCompatActivity {
         chart.invalidate();
     }
 
-
-    private void lineChart2(){
+    private void lineChart2() {
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
 
         for (int z = 0; z < 3; z++) {
